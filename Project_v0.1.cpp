18f54cd37a06f11f23df022b88c757b79dede582
emuk213 <emilija.smile01@gmail.com>
2024-09-24 21:24:31 +0300

    main
    pradinis failas

---
+++ Project_v0.1.cpp
@@ -0,0 +1,16 @@
+// Project_v0.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "MyLib.h"
+#include "Stud.h"
+
+
+int main()
+{
+   
+    system("pause");
+
+    return 0;
+}
+
