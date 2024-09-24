12dd9163321b417bd199f55204d0760962cb827d
emuk213 <emilija.smile01@gmail.com>
2024-09-24 21:27:31 +0300

    kaip naudoti koda
    jame bus aprasyta kaip skaityti programa

---
+++ MyLib.h
@@ -0,0 +1,15 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+
+#endif // MYLIB_H_INCLUDED
+
