e88d23bd4087367900827a32e3ded5c83b48cc47
emuk213 <emilija.smile01@gmail.com>
2024-09-24 21:40:19 +0300

    sukurtos reikalingos funkcijos
    sukuriau isvedimo, ivedimo, valymo funkcijas. be to dar galutinio balo skaiciavimo funkcija, kai nd yra vienas int balas

--- Stud.cpp
+++ Stud.cpp
@@ -1,4 +1,21 @@
 #include "Stud.h"
 
+void skaiciuotiGalutiniBala(Stud& Lok) {
+	Lok.vid = 0.4 * Lok.nd + 0.6 * Lok.egz;
+}
+
+void input(Stud& Lok) {
+	cout << "Input Name, Surname, ND and Exam score:";
+	cin >> Lok.vardas >> Lok.pavarde >> Lok.nd >> Lok.egz;
+}
+
+void output(Stud Lok) {
+	cout << setw(15) << left << Lok.pavarde << setw(15) << left << Lok.vardas << setw(5) << setprecision(2) << fixed << right << Lok.vid << endl;
+}
+
+void clean(Stud& Lok) {
+	Lok.vardas.clear();
+	Lok.pavarde.clear();
+}
 
 
