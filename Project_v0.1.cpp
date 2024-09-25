f1881466156036ffebd056c094b45fc571cfbf36
emuk213 <emilija.smile01@gmail.com>
2024-09-25 17:26:52 +0300

    Suteikiau galimybę vartotojui pasirinkti ar jis nori apskaičiuoto galutinio balo su vidurkiu ar su mediana.

--- Project_v0.1.cpp
+++ Project_v0.1.cpp
@@ -7,8 +7,10 @@ int main()
 {
     vector <Stud> vec1;
     Stud temp;
+    int a, n;
+    cout << "Do you want Final score with vid or med? Type 0 for vid, 1 for med";
+    cin >> a;
     cout << "How many students do you have?";
-    int n;
     cin >> n;
     for (int i = 0; i < n; i++) {
         cout << "Please input student data:" << endl;
@@ -17,12 +19,18 @@ int main()
         vec1.push_back(temp);
         clean(temp);
     }
-    cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid)" << endl;
-    for (int i = 0; i < n; i++)
-        output(vec1.at(i));
+    if (a == 0) {
+        cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
+        for (int i = 0; i < n; i++)
+            outputVid(vec1.at(i));
+    }
+    else {
+        cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
+        for (int i = 0; i < n; i++)
+            outputMed(vec1.at(i));
+    }
 
     system("pause");
 
     return 0;
 }
-
