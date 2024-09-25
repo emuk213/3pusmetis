0670da80feaaecb7c08e5e0618b252716d533b07
emuk213 <emilija.smile01@gmail.com>
2024-09-26 01:18:16 +0300

    Įtraukiu reikalingas bibliotekas ir pavadinimus

--- MyLib.h
+++ MyLib.h
@@ -6,6 +6,8 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+#include <fstream>
+#include <sstream> //del getline
 #include <ios> //(del streamsize)
 #include <limits> //(del numeric limits)
 #include <cstdlib> //(del rand)
@@ -19,11 +21,14 @@ using std::setw;
 using std::setprecision;
 using std::fixed;
 using std::sort;
+using std::max;
+using std::ifstream;
 using std::streamsize;
 using std::numeric_limits;
 using std::string;
 using std::vector;
-
+using std::runtime_error;
+using std::istringstream;
+using std::cerr;
 
 #endif // MYLIB_H_INCLUDED
-//la
