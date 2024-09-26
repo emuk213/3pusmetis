26ba2364baf9bd41a246ca81ad5751b208beb11c
emuk213 <emilija.smile01@gmail.com>
2024-09-26 20:45:26 +0300

    Parašiau išimčių valdymą vartotojo įvedimams.

--- Project_v0.1/Project_v0.1.cpp
+++ Project_v0.1/Project_v0.1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "MyLib.h"
 #include "Stud.h"
 
@@ -10,56 +11,81 @@ int main()
     int a, n, b;
     string failoVardas;
     cout << "Do you want Final score with vid or med (0 - vid, 1 - med)?" << endl;
-    cin >> a;
-    cout << "Do you want to read data from txt file (1 - yes, 0 - no)?" << endl;
-    cin >> b;
-    if (b == 1) {
-        cout << "Input file name" << endl;
-        cin >> failoVardas;
-        try {
-            readStudTxt(failoVardas, vec1);
-        catch (const runtime_error& e) {
-            cerr << e.what() << endl;
-            return 1;  // Exit if there's an error
-        }
-        if (a == 0) {
-            cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
-            for (Stud& student : vec1) {
-                skaiciuotiGalutiniBala(student);
-                outputVid(student);
-            }
-        }
-        else if (a == 1) {
-            cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
-            for (Stud& student : vec1) {
-                skaiciuotiGalutiniBala(student);
-                outputMed(student);
-            }
+    try {
+        cin >> a;
+        if (cin.fail() || (a != 0 && a != 1)) {
+            throw runtime_error("Error: wrong input");
         }
 
-    }
-    else if(b == 0){
-        cout << "How many students do you have?" << endl;
-        cin >> n;
-        for (int i = 0; i < n; i++) {
-            cout << "Please input student data:" << endl;
-            input(temp);
-            skaiciuotiGalutiniBala(temp);
-            vec1.push_back(temp);
-            clean(temp);
-        }
-        if (a == 0) {
-            cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
-            for (int i = 0; i < n; i++)
-                outputVid(vec1.at(i));
-        }
-        else if (a == 1){
-            cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
-            for (int i = 0; i < n; i++)
-                outputMed(vec1.at(i));
+        cout << "Do you want to read data from txt file (0 - no, 1 - yes)?" << endl;
+
+        cin >> b;
+        if (cin.fail() || (b != 0 && b != 1)) {
+            throw runtime_error("Error: wrong input");
         }
         
+
+
+        if (b == 1) {
+            cout << "Input file name" << endl;
+            cin >> failoVardas;
+            
+            readStudTxt(failoVardas, vec1);
+            
+            if (a == 0) {
+                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
+                for (Stud& student : vec1) {
+                    skaiciuotiGalutiniBala(student);
+                    outputVid(student);
+                }
+            }
+            else if (a == 1) {
+                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
+                for (Stud& student : vec1) {
+                    skaiciuotiGalutiniBala(student);
+                    outputMed(student);
+                }
+            }
+
+            /* Atspausdina duomenis is txt file
+               outputTxt(vec1);
+            */
+        }
+
+
+
+        else if (b == 0) {
+            cout << "How many students do you have?" << endl;
+            
+            cin >> n;
+            if (cin.fail()) {
+                throw runtime_error("Error: input must be a number");
+            }
+            
+            for (int i = 0; i < n; i++) {
+                cout << "Please input student data:" << endl;
+                input(temp);
+                skaiciuotiGalutiniBala(temp);
+                vec1.push_back(temp);
+                clean(temp);
+            }
+            if (a == 0) {
+                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
+                for (int i = 0; i < n; i++)
+                    outputVid(vec1.at(i));
+            }
+            else if (a == 1) {
+                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
+                for (int i = 0; i < n; i++)
+                    outputMed(vec1.at(i));
+            }
+
+        }
     }
+     catch (exception& e) {
+         cerr << e.what() << endl;
+         return 1;
+     }
 
     system("pause");
 
